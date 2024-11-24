54571c6183c5f6bad12ab6aefcbff6cdf68ce8de
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2024-11-24 11:21:56 +0000

    Edited the parent component

--- animalgame_app/src/App.js
+++ animalgame_app/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import AnimalName from "./components/AnimalName";
+import AnimalGrid from "./components/AnimalGrid";
+import Result from "./components/Result";
+import { animals } from "./data/AnimalsDb";
 
 function App() {
+  const [randomAnimal, setRandomAnimal] = useState("");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * animals.length);
+    setRandomAnimal(animals[randomIndex].name);
+  }, []);
+
+  const handleAnimalClick = (animalName) => {
+    if (animalName === randomAnimal) {
+      setResult("Win");
+    } else {
+      setResult("Lose");
+    }
+  };
+
   return (
     <div className="App">
-      
+      <Header />
+      <AnimalName animalName={randomAnimal} />
+      <AnimalGrid animals={animals} onAnimalClick={handleAnimalClick} />
+      {result && <Result result={result} />}
     </div>
   );
 }
